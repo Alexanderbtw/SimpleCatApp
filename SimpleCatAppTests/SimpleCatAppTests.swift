92e96e38b9ea8a82b891b12e5d4da9799275be2d
Alexanderbtw <sgoldebaev@gmail.com>
2024-11-04 01:15:54 +0300

    Initial Commit

---
+++ SimpleCatAppTests/SimpleCatAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCatAppTests.swift
+//  SimpleCatAppTests
+//
+//  Created by Alexander Goldebaev on 11/4/24.
+//
+
+import Testing
+@testable import SimpleCatApp
+
+struct SimpleCatAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
